0eb5fc69fe9a838f209fae50f9f35e322082e2bf
Szymon Kaniewski <skaniewski@altimetrik.com>
2022-11-22 13:02:18 +0100

    fixed cache issue

--- api-service/src/main/java/test/helm/service/TimeService.java
+++ api-service/src/main/java/test/helm/service/TimeService.java
@@ -1,19 +1,26 @@
 package test.helm.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class TimeService {
 
-  private final RestTemplate restTemplate;
   private String url;
 
   @Autowired
@@ -24,10 +31,25 @@ public class TimeService {
     url = uri + path;
   }
 
+  @SneakyThrows({
+      URISyntaxException.class,
+      IOException.class,
+      InterruptedException.class
+  })
   public String currentTime() {
     log.info("Invoking: {}", url);
-    return restTemplate
-        .getForEntity(url, String.class)
-        .getBody();
+
+    HttpClient client = HttpClient.newBuilder()
+        .version(Version.HTTP_2)
+        .followRedirects(Redirect.NORMAL)
+        .build();
+
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(new URI(url))
+        .GET()
+        .timeout(Duration.ofSeconds(1))
+        .build();
+
+    return client.send(request, BodyHandlers.ofString()).body();
   }
 }
