710af64840f789352cda0c49b4b838e1d898a547
Szymon Kaniewski <skaniewski@altimetrik.com>
2022-12-02 16:47:36 +0100

    directory mount

--- time-service/src/main/java/test/helm/controller/TimeController.java
+++ time-service/src/main/java/test/helm/controller/TimeController.java
@@ -3,6 +3,7 @@ package test.helm.controller;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import test.helm.service.TimeService;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/time")
@@ -26,7 +28,9 @@ public class TimeController {
 
   @GetMapping
   public String currentTime() {
-    return podPrefix + service.currentTime();
+    String currentTime = service.currentTime();
+    log.debug("currentTime() = {}", currentTime);
+    return podPrefix + currentTime;
   }
 
 }
